428a797d0bcd026f834be38534d13b1a393019a7
Seba <245787@edu.p.lodz.pl>
2025-03-17 22:11:37 +0100

    DOdanie nowego Layoutu i dodanie zastąpienie poprzedniego MainActivity.kt, zmaina poprzedniego MainActivity.kt na EasyCalcActivity.kt

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,169 +1,40 @@
 package com.example.calculator
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
-    private var isDotExistInNumber: Boolean = false
-    private lateinit var splitData: List<String>
-    private lateinit var buttonClear: Button
-    private lateinit var buttonDelete: Button
-    private lateinit var buttonDivide: Button
-    private lateinit var buttonMultiply: Button
-    private lateinit var buttonAdd: Button
-    private lateinit var buttonSubtract: Button
-    private lateinit var buttonResult: Button
-    private lateinit var buttonOne: Button
-    private lateinit var buttonTwo: Button
-    private lateinit var buttonThree: Button
-    private lateinit var buttonFour: Button
-    private lateinit var buttonFive: Button
-    private lateinit var buttonSix: Button
-    private lateinit var buttonSeven: Button
-    private lateinit var buttonEight: Button
-    private lateinit var buttonNine: Button
-    private lateinit var buttonZero: Button
-    private lateinit var buttonDot: Button
-    private lateinit var resultView: TextView
 
-    @SuppressLint("SetTextI18n")
+    private lateinit var buttonEasyCalc : Button
+    private lateinit var buttonAdvancedCalc : Button
+    private lateinit var buttonAboutMe : Button
+    private lateinit var buttonExit : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setLayoutObjects()
+        setObjects()
 
-        resultView.text = "0"
-        buttonZero.setOnClickListener { addSignToResultView("0") }
-        buttonOne.setOnClickListener { addSignToResultView("1") }
-        buttonTwo.setOnClickListener { addSignToResultView("2") }
-        buttonThree.setOnClickListener { addSignToResultView("3") }
-        buttonFour.setOnClickListener { addSignToResultView("4") }
-        buttonFive.setOnClickListener { addSignToResultView("5") }
-        buttonSix.setOnClickListener { addSignToResultView("6") }
-        buttonSeven.setOnClickListener { addSignToResultView("7") }
-        buttonEight.setOnClickListener { addSignToResultView("8") }
-        buttonNine.setOnClickListener { addSignToResultView("9") }
-        buttonAdd.setOnClickListener { addSignToResultView(" + ") }
-        buttonSubtract.setOnClickListener { addSignToResultView(" - ") }
-        buttonDivide.setOnClickListener { addSignToResultView(" / ") }
-        buttonMultiply.setOnClickListener { addSignToResultView(" * ") }
-        buttonResult.setOnClickListener {
-            if (validateData()) resultView.text =
-                resultView.text.toString() + "\n= " + countResult()
+        buttonEasyCalc.setOnClickListener {
+            val intent = Intent(this, EasyCalcActivity::class.java)
+            startActivity(intent)
         }
-        buttonDelete.setOnClickListener {
-            if (resultView.text.length == 1) resultView.text = "0"
-            else {
-                if (!isLastSignNumber()) resultView.text =
-                    resultView.text.substring(0, resultView.text.length - 3)
-                else resultView.text = resultView.text.substring(0, resultView.text.length - 1)
-            }
-        }
-        buttonClear.setOnClickListener { resultView.text = "0" }
-        buttonDot.setOnClickListener {
-            if (!isDotExistInNumber && isLastSignNumber()) {
-                addSignToResultView(".")
-            }
+
+        buttonAdvancedCalc.setOnClickListener {  }
+        buttonAboutMe.setOnClickListener {  }
+        buttonExit.setOnClickListener {
+            finishAffinity()
+            exitProcess(0)
         }
     }
 
-    private fun setLayoutObjects() {
-        buttonZero = findViewById(R.id.buttonZero)
-        buttonOne = findViewById(R.id.buttonOne)
-        buttonTwo = findViewById(R.id.buttonTwo)
-        buttonThree = findViewById(R.id.buttonThree)
-        buttonFour = findViewById(R.id.buttonFour)
-        buttonFive = findViewById(R.id.buttonFive)
-        buttonSix = findViewById(R.id.buttonSix)
-        buttonSeven = findViewById(R.id.buttonSeven)
-        buttonEight = findViewById(R.id.buttonEight)
-        buttonNine = findViewById(R.id.buttonNine)
-        buttonAdd = findViewById(R.id.buttonAdd)
-        buttonSubtract = findViewById(R.id.buttonSubtract)
-        buttonMultiply = findViewById(R.id.buttonMultiply)
-        buttonDivide = findViewById(R.id.buttonDivide)
-        buttonDelete = findViewById(R.id.buttonDelete)
-        buttonClear = findViewById(R.id.buttonClear)
-        buttonResult = findViewById(R.id.buttonResult)
-        buttonDot = findViewById(R.id.buttonDot)
-        resultView = findViewById(R.id.resultField)
-    }
-
-    @SuppressLint("SetTextI18n")
-    private fun addSignToResultView(sign: String) {
-        if (resultView.text.length == 1 && resultView.text == "0" && (sign >= "0" && sign <= "9")) {
-            resultView.text = sign
-            return
-        }
-        if (sign == ".") isDotExistInNumber = true
-        else if(!(sign >= "0" && sign <= "9")) isDotExistInNumber = false
-        resultView.text = resultView.text.toString() + sign
-
-    }
-
-    private fun isLastSignNumber(): Boolean {
-        return ((resultView.text.last() in '0'..'9'))
-    }
-
-    private fun displayToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun validateData(): Boolean {
-        if (!isLastSignNumber() || resultView.text.last() == '.') {
-            displayToast("Obliczenia nie mogą kończyć się operacją bądz przecinkiem!!")
-            return false
-        }
-        splitData = resultView.text.split(" ")
-        for (i in splitData.indices) {
-            if (splitData[i] == "/" && splitData[i + 1].toDouble() == 0.0) {
-                displayToast("W obliczeniach znajduje się zabronione dzielenie przez 0!!")
-                return false
-            }
-        }
-        return true
-    }
-
-    private fun countResult(): String {
-        while (true) {
-            if (splitData.size == 1) break
-            var isCounted = false
-            for (i in splitData.indices) {
-                if (splitData[i] == "*" || splitData[i] == "/") {
-                    val previousValue: Double = splitData[i - 1].toDouble()
-                    val nextValue: Double = splitData[i + 1].toDouble()
-                    val result: Double = if (splitData[i] == "*") previousValue * nextValue
-                    else previousValue / nextValue
-                    splitData = splitData.toMutableList().apply {
-                        removeAt(i + 1)
-                        removeAt(i)
-                        set(i - 1, result.toString())
-                    }
-                    isCounted = true
-                    break
-                }
-            }
-            if (!isCounted) {
-                for (i in splitData.indices) {
-                    if (splitData[i] == "+" || splitData[i] == "-") {
-                        val previousValue: Double = splitData[i - 1].toDouble()
-                        val nextValue: Double = splitData[i + 1].toDouble()
-                        val result: Double = if (splitData[i] == "+") previousValue + nextValue
-                        else previousValue - nextValue
-                        splitData = splitData.toMutableList().apply {
-                            removeAt(i + 1)
-                            removeAt(i)
-                            set(i - 1, result.toString())
-                        }
-                        break
-                    }
-                }
-            }
-        }
-        return splitData[0]
+    fun setObjects(){
+        buttonEasyCalc = findViewById(R.id.buttonEasyCalc)
+        buttonAdvancedCalc = findViewById(R.id.buttonAdvanced)
+        buttonAboutMe = findViewById(R.id.buttonAboutMe)
+        buttonExit = findViewById(R.id.buttonExit)
     }
 }
